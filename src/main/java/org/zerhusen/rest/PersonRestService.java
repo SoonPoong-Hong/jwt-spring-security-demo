ff36d90d8f4552b85def5a8cedd3e93301ee4a41
Hong SoonPoong <rocklike@nexon.co.kr>
2018-11-27 13:10:40 +0900

    .

--- src/main/java/org/zerhusen/rest/PersonRestService.java
+++ src/main/java/org/zerhusen/rest/PersonRestService.java
@@ -1,12 +1,13 @@
 package org.zerhusen.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class PersonRestService {
@@ -30,6 +31,10 @@ public class PersonRestService {
                 .findAny().orElse(null);
     }
     
+    @GetMapping("/everyone")
+    public Dummy dummy() {
+    	return new Dummy();
+    }
     
     class Dummy{
     	public String name = "Hong";
